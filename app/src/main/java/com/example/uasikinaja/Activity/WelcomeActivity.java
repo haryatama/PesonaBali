80eea5e90d38fd21c4d1eedbda2d2c342a38bdb0
haryatama10 <haryatama420@gmail.com>
2019-12-18 18:17:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uasikinaja/Activity/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.uasikinaja.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uasikinaja.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
